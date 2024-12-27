677c069453031503d6b631d8e39cbb6e7f0add6e
Verone Lazio <verone.lazio@gmail.com>
2024-12-27 12:56:43 -0500

    Update Wezterm config: optimize as tmux host with Catppuccin theme Configure as minimal tmux host with font and window settings Apply Catppuccin Mocha theme to match tmux colors Add essential features: cursor settings, scrollback, link handling Remove redundant features handled by tmux (tabs, panes) Set MesloLGS Nerd Font with optimal size and opacity

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,40 +1,79 @@
-local wezterm = require 'wezterm'
+local wezterm = require('wezterm')
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- Color scheme and appearance
-config.color_scheme = 'Catppuccin Mocha'
-config.enable_tab_bar = false
-config.font_size = 16.0
-config.font = wezterm.font('MesloLGS Nerd Font Mono')  -- Changed to Nerd Font for better icons
-config.macos_window_background_blur = 30
-config.window_background_opacity = 1.0
-config.window_decorations = 'RESIZE'
+-- Font configuration
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font_size = 19
 
--- Key bindings
+-- Window appearance
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+config.window_close_confirmation = "NeverPrompt"
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 10
+
+-- Catppuccin Mocha colors
+config.colors = {
+    foreground = "#CDD6F4",
+    background = "#1E1E2E",
+    cursor_bg = "#F5E0DC",
+    cursor_border = "#F5E0DC",
+    cursor_fg = "#1E1E2E",
+    selection_bg = "#585B70",
+    selection_fg = "#CDD6F4",
+    ansi = {
+        "#45475A", -- black
+        "#F38BA8", -- red
+        "#A6E3A1", -- green
+        "#F9E2AF", -- yellow
+        "#89B4FA", -- blue
+        "#F5C2E7", -- magenta
+        "#94E2D5", -- cyan
+        "#BAC2DE", -- white
+    },
+    brights = {
+        "#585B70", -- bright black
+        "#F38BA8", -- bright red
+        "#A6E3A1", -- bright green
+        "#F9E2AF", -- bright yellow
+        "#89B4FA", -- bright blue
+        "#F5C2E7", -- bright magenta
+        "#94E2D5", -- bright cyan
+        "#A6ADC8", -- bright white
+    },
+}
+
+-- Basic key bindings (minimal since we're using tmux)
 config.keys = {
     {
         key = 'f',
         mods = 'CTRL',
         action = wezterm.action.ToggleFullScreen,
     },
-    {
-        key = '\'',
-        mods = 'CTRL',
-        action = wezterm.action.ClearScrollback 'ScrollbackAndViewport',
-    },
 }
 
 -- Mouse bindings
 config.mouse_bindings = {
-    -- Ctrl-click will open the link under the mouse cursor
+    -- Ctrl-click will open links
     {
         event = { Up = { streak = 1, button = 'Left' } },
         mods = 'CTRL',
         action = wezterm.action.OpenLinkAtMouseCursor,
     },
+    -- Right click paste
+    {
+        event = { Down = { streak = 1, button = "Right" } },
+        mods = "NONE",
+        action = wezterm.action.PasteFrom 'Clipboard',
+    },
 }
 
--- Return the configuration
+-- Terminal Features
+config.scrollback_lines = 10000
+config.default_cursor_style = 'BlinkingBlock'
+config.cursor_blink_rate = 800
+config.cursor_blink_ease_in = 'Linear'
+config.cursor_blink_ease_out = 'Linear'
+
 return config
