99c0b08ecfb2634bf3b07694c95a5c969849cc50
Verone Lazio <verone.lazio@gmail.com>
2024-12-27 12:06:22 -0500

    Update Wezterm config: combine developer guide settings with repository features

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,42 +1,40 @@
 local wezterm = require 'wezterm'
-return {
-	-- color_scheme = 'termnial.sexy',
-	color_scheme = 'Catppuccin Mocha',
-	enable_tab_bar = false,
-	font_size = 16.0,
-	font = wezterm.font('JetBrains Mono'),
-	-- macos_window_background_blur = 40,
-	macos_window_background_blur = 30,
-	
-	-- window_background_image = '/Users/omerhamerman/Downloads/3840x1080-Wallpaper-041.jpg',
-	-- window_background_image_hsb = {
-	-- 	brightness = 0.01,
-	-- 	hue = 1.0,
-	-- 	saturation = 0.5,
-	-- },
-	-- window_background_opacity = 0.92,
-	window_background_opacity = 1.0,
-	-- window_background_opacity = 0.78,
-	-- window_background_opacity = 0.20,
-	window_decorations = 'RESIZE',
-	keys = {
-		{
-			key = 'f',
-			mods = 'CTRL',
-			action = wezterm.action.ToggleFullScreen,
-		},
-		{
-			key = '\'',
-			mods = 'CTRL',
-			action = wezterm.action.ClearScrollback 'ScrollbackAndViewport',
-		},
-	},
-	mouse_bindings = {
-	  -- Ctrl-click will open the link under the mouse cursor
-	  {
-	    event = { Up = { streak = 1, button = 'Left' } },
-	    mods = 'CTRL',
-	    action = wezterm.action.OpenLinkAtMouseCursor,
-	  },
-	},
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- Color scheme and appearance
+config.color_scheme = 'Catppuccin Mocha'
+config.enable_tab_bar = false
+config.font_size = 16.0
+config.font = wezterm.font('MesloLGS Nerd Font Mono')  -- Changed to Nerd Font for better icons
+config.macos_window_background_blur = 30
+config.window_background_opacity = 1.0
+config.window_decorations = 'RESIZE'
+
+-- Key bindings
+config.keys = {
+    {
+        key = 'f',
+        mods = 'CTRL',
+        action = wezterm.action.ToggleFullScreen,
+    },
+    {
+        key = '\'',
+        mods = 'CTRL',
+        action = wezterm.action.ClearScrollback 'ScrollbackAndViewport',
+    },
 }
+
+-- Mouse bindings
+config.mouse_bindings = {
+    -- Ctrl-click will open the link under the mouse cursor
+    {
+        event = { Up = { streak = 1, button = 'Left' } },
+        mods = 'CTRL',
+        action = wezterm.action.OpenLinkAtMouseCursor,
+    },
+}
+
+-- Return the configuration
+return config
